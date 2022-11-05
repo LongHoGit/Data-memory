d37e60040615e814703588988a30d4d5b0991b1c
LongHo <93377075+LongHoGit@users.noreply.github.com>
2022-11-05 19:51:50 +0700

    Add files via upload

--- data_mem.sv
+++ data_mem.sv
@@ -2,7 +2,7 @@ module data_mem (
   //input
   input  logic        rst_ni,
   input  logic        clk_i,
-  input  logic [7:0]  addr_i,// memory addr_i
+  input  logic [9:0]  addr_i,// memory addr_i
   input  logic [31:0] wdata_i, //the write data
   input  logic        wren_i, //write enable
 
@@ -10,7 +10,7 @@ module data_mem (
   output logic [31:0] r_data_o //the read data
 );
 
-  logic [31:0] mem[255:0];
+  logic [31:0] mem[1023:0];
 
   initial $writememh("C:/altera/13.0sp1/lab3_computer_architecture/memory/datamem.data",mem,0,4);
   
