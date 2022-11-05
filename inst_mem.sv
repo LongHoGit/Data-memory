e7ab2ee162620d3ed53bbd1fc8f30c1d0f1a9de3
LongHo <93377075+LongHoGit@users.noreply.github.com>
2022-11-05 19:50:34 +0700

    Add files via upload

--- inst_mem.sv
+++ inst_mem.sv
@@ -2,17 +2,17 @@ module inst_mem (
   //input
   //input rst_ni,
   //input clk_i,
-  input  logic [13:0] addr,
+  input  logic [13:0] addr_i,
 
   //output
-  output logic [31:0] r_data
+  output logic [31:0] r_data_o
 );
 
 logic [31:0] mem[16383:0];
 
-initial $readmemb("C:/altera/13.0sp1/lab3_computer_architecture/memory/instmem.data",mem);
+initial $readmemh("C:/altera/13.0sp1/lab3_computer_architecture/memory/instmem.data",mem);
 
-always_ff @(addr)
-	r_data <= mem[addr];
+always_ff @(addr_i)
+	r_data_o <= mem[addr_i];
 
 endmodule : inst_mem
